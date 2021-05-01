d032935e4f77908ba7996184e1a24a4c1e7e1b3c
Aakarsh Kumar Verma <aakarshkun@gmail.com>
2021-05-01 12:19:48 +0530

    Update settings.py

--- eventapp/eventapp/settings.py
+++ eventapp/eventapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c=idme4m#b=*2(zsc+r123jtmgvgp99e03eg)_f0e$di86lm+y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
